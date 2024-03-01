2f2be58a0a945fc4a92c5183941f6b49bd9b2fd6
Jakub Piwowarski <jakubpiwowarski@10g.pl>
2024-03-01 12:30:42 +0100

    Add files via upload

--- drive.py
+++ drive.py
@@ -1,38 +1,36 @@
 import socketio
 import eventlet
-import numpy as np
 from flask import Flask
 from keras.models import load_model
 import base64
 from io import BytesIO
 from PIL import Image
+import numpy as np
 import cv2
 
 sio = socketio.Server()
+app = Flask(__name__)
 
-app = Flask(__name__) #'__main__'
 speed_limit = 10
-def img_preprocess(img):
-    img = img[60:135,:,:]
-    img = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
-    img = cv2.GaussianBlur(img,  (3, 3), 0)
-    img = cv2.resize(img, (200, 66))
-    img = img/255
-    return img
-
+def image_preprocess(img):
+  img = img[60:135,:,:]
+  img = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
+  img = cv2.GaussianBlur(img, (3,3), 0)
+  img = cv2.resize(img, (200, 66))
+  img = img/255
+  return img
 
 @sio.on('telemetry')
 def telemetry(sid, data):
     speed = float(data['speed'])
     image = Image.open(BytesIO(base64.b64decode(data['image'])))
     image = np.asarray(image)
-    image = img_preprocess(image)
+    image = image_preprocess(image)
     image = np.array([image])
     steering_angle = float(model.predict(image))
     throttle = 1.0 - speed/speed_limit
-    print('{} {} {}'.format(steering_angle, throttle, speed))
+    print('{} {} {}'.format(steering_angle,throttle,speed))
     send_control(steering_angle, throttle)
-
 
 
 @sio.on('connect')
@@ -45,7 +43,6 @@ def send_control(steering_angle, throttle):
         'steering_angle': steering_angle.__str__(),
         'throttle': throttle.__str__()
     })
-
 
 if __name__ == '__main__':
     model = load_model('model.h5')
